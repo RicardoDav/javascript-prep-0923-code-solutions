2f24b25efdac5acd80e7a1fe81bc11d754c2f8b6
Ricardo Davila <rdavila@globalnt.com>
2023-10-07 17:07:52 +0000

    Completed Prep-Javascript-Functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,57 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+const addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('Add two numbers exercise: ' + addTwoNumbersResult);
+function convertHourstoMinutes(hours) {
+  return hours * 60;
+}
+const Hours = convertHourstoMinutes(2);
+console.log('Hours to Minutes exercise: ' + Hours);
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+const Label = getGreeting('World');
+console.log('Get greeting exercise: ' + Label);
+function addAndMultiply(num1, num2) {
+  const sum = num1 + num2;
+  const result = sum * 5;
+  return result;
+}
+const Label2 = addAndMultiply(10, 5);
+console.log('Add and multiply exercise: ' + Label2);
+function multiplyAndDivideBy5(num1, num2) {
+  const mult = num1 * num2;
+  const result2 = mult / 5;
+  return result2;
+}
+const Label3 = multiplyAndDivideBy5(35, 10);
+console.log('Multiply and Divide exercise: ' + Label3);
+function substractTwoNumbers(num1, num2) {
+  const subst = num1 - num2;
+
+  return subst;
+}
+const Label4 = substractTwoNumbers(22, 7);
+console.log('substract two numbers exercise: ' + Label4);
+function getCircleCircumference(num1) {
+  const circ = num1 * 2 * 3.1416;
+
+  return circ;
+}
+const Label5 = getCircleCircumference(5);
+console.log('Get Circle Circunference exercise: ' + Label5);
+
+function getFullName(firstName, lastName) {
+  const fullName = firstName + ' ' + lastName;
+
+  return fullName;
+}
+const Label6 = getFullName('Juan', 'Ramirez');
+console.log('Get Full Name exercise: ' + Label6);
+
+function cube(num1) {
+  return num1 * num1 * num1;
+}
+const Label7 = cube(5);
+console.log('GetCube exercixe ' + Label7);
