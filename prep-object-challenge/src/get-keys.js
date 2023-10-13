c7f77fc30ab709b6c3e6cff48a2d85bbbc4fa6a1
Ricardo Davila <rdavila@globalnt.com>
2023-10-13 20:42:20 +0000

    Completed prep-object-challenge

---
+++ prep-object-challenge/src/get-keys.js
@@ -0,0 +1,12 @@
+/* exported getKeys */
+function getKeys(object) {
+  const keys = [];
+
+  for (const key in object) {
+    if (object[key]) {
+      keys.push(key);
+    }
+  }
+
+  return keys;
+}
